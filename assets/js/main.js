a8446b7eee0984ff8b8a4641456882c7a4fb9a6f
wealcodemizan <wealcode.mizan@gmail.com>
2021-05-24 05:09:49 -0700

    Main Menu Completed.

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+
+    // counter up
+    // $('.counter').counterUp();
+
+
+    // Category Slider
+    $('.cate-slider').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:true,
+        dots: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:7
+          }
+        }
+    });
+
+
+    // Popular Curses Slider
+    $('.popular-slider').owlCarousel({
+        loop:true,
+        margin:30,
+        autoplay: true,
+        smartSpeed: 1000,
+        autoplayHoverPause: true,
+        dots: false,
+        nav:true,
+        responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+        }
+    });
+
+
+
+    
+
+
+});
+
+
+// Stricky Menu
+var sticky_menu = document.getElementById("sticky_menu");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    sticky_menu.classList.add('sticky_menu');
+  } else {
+    sticky_menu.classList.remove('sticky_menu');
+  }
+}
